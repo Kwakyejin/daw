5550a943e6a76b0850665efa1cb26ffb67e27c43
Yejin_Kwak <ng4812@naver.com>
2022-08-15 19:04:41 +0900

    intro2 complete

--- screen/Intro2.js
+++ screen/Intro2.js
@@ -1,20 +1,36 @@
 import React from "react";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View, Button } from "react-native";
 import {LinearGradient} from 'expo-linear-gradient';
 import { WithLocalSvg } from "react-native-svg";
-import logo from "../assets/Group.svg"
+import logo from "../assets/Group-2.svg"
+import title from "../assets/dawtitle.svg"
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Intro2 = () => (
   <View style={styles.container}>
     <ImageBackground source={require("../assets/map.png")} resizeMode="cover" style={styles.image}>
     <LinearGradient colors={['black', 'rgba(255,255,255,0)']} style={styles.container}>
-    <Text style={{color:"white",fontSize: 20, fontWeight:"300", marginRight:150, fontFamily:"Comfortaa-VariableFont_wght"}}>daw,</Text>
-    <Text style={{color:"white",fontSize: 20, fontWeight:"600"}}>지도를 보는 또 다른 관점</Text>
+      <View style ={{marginBottom:"20%"}}>
+    <WithLocalSvg
+            width={200}
+            height={40}
+            asset={title}
+        />
     <WithLocalSvg
             width={200}
             height={107}
             asset={logo}
+            shadowColor = "#000000"
+            shadowOpacity= {0.5}
         />
+        </View>
+      <View style={{justifyContent:"flex-end",position : "absolute", bottom: 50 }}>
+      <TouchableOpacity>
+      <View style={{ height:52, width:315, backgroundColor:"#292929", alignItems:"center", justifyContent:"center", borderRadius:10}}>
+        <Text style={{color:"white"}}>LOG IN</Text>
+      </View>
+      </TouchableOpacity>
+      </View>
     </LinearGradient>
     </ImageBackground>
   </View>
@@ -23,6 +39,8 @@ const Intro2 = () => (
 const styles = StyleSheet.create({
 container: {
         flex: 1,
+        height:"100%",
+        width:"100%",
         justifyContent:"center",
         alignItems:"center",
     },
@@ -32,12 +50,6 @@ container: {
     height:"100%",
     alignContent:"center",
     justifyContent: "center"
-  },
-  linearGradient: {
-    flex: 1,
-    paddingLeft: 15,
-    paddingRight: 15,
-    borderRadius: 5
   },
 });
 
